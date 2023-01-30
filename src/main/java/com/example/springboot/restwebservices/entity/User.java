d5f5043cafbe940e55f9bf62b0fd220d635ba98f
Pratyush Gupta <Pratyush.Gupta@cashe.local>
2023-01-30 11:44:54 +0530

    first

---
+++ src/main/java/com/example/springboot/restwebservices/entity/User.java
@@ -0,0 +1,58 @@
+package com.example.springboot.restwebservices.entity;
+
+import jakarta.persistence.*;
+
+//created a model class of users, which will be stored in a user table with the following fields
+@Entity     //This is responsible for converting object into a table
+@Table(name="users")            //Table name
+public class User {
+
+    @Id        //setting the primary key
+    @GeneratedValue(strategy = GenerationType.AUTO)  //auto, what does it do exactly?
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
